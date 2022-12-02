6e95fcea0010979fa3d52cf7d3c806ae89aa80a3
GonzaloMonteodorisio <56704895+GonzaloMonteodorisio@users.noreply.github.com>
2022-12-02 13:03:40 -0300

    BOM

---
+++ BOM-Objetos/index.js
@@ -0,0 +1,37 @@
+// Objetos: URL, Historial y Navegador 
+
+// URL (location)
+console.info('***** Objeto URL (location) *****');
+console.info('location: ', location);
+console.info('location.origin: ', location.origin);
+console.info('location.protocol: ', location.protocol);
+console.info('location.host: ', location.host);
+console.info('location.hostname: ', location.hostname);
+console.info('location.port: ', location.port);
+console.info('location.href: ', location.href);
+console.info('location.hash: ', location.hash);
+console.info('location.search: ', location.search);
+console.info('location.pathname: ', location.pathname);
+// location.reload();
+
+// Historial  (history)
+console.info('***** Objeto Historial (history) *****');
+console.info('history: ', history);
+console.info('history.length: ', history.length);
+// history.back(numberOfPages);
+// history.forward(numberOfPages);
+// history.go(numberOfPages, negative, 0 or positive);
+
+// Navegador (navigator) 
+console.info('***** Objeto Navegador (navigator) *****');
+console.info('navigator: ', navigator);
+console.info('navigator.connection: ', navigator.connection);
+console.info('navigator.geolocation: ', navigator.geolocation);
+console.info('navigator.mediaDevices: ', navigator.mediaDevices);
+console.info('navigator.mimeTypes: ', navigator.mimeTypes);
+console.info('navigator.onLine: ', navigator.onLine);
+console.info('navigator.serviceWorker: ', navigator.serviceWorker);
+console.info('navigator.storage: ', navigator.storage);
+console.info('navigator.usb: ', navigator.usb);
+console.info('navigator.userAgent: ', navigator.userAgent);
+
--- BOM/index.js
+++ BOM/index.js
@@ -1,6 +1,5 @@
 console.info('window: ', window);
 
-
 // Resize Event
 window.addEventListener('resize', e => {
   console.clear();
@@ -13,19 +12,28 @@ window.addEventListener('resize', e => {
 });
 
 
-// Scroll Event (not working !!!)
-window.addEventListener('scroll', e => {
-  console.clear();
-  console.info('***** Evento Scroll *****');
-  console.info('window.scrollX: ', window.scrollX);
-  console.info('window.scrollY: ', window.scrollY);
-  console.info('event - scroll: ', e);
-});
+// Scroll Event (no funciona bien en Chrome, sí en Firefox !!!)
+// window.addEventListener('scroll', e => {
+//   console.clear();
+//   console.info('***** Evento Scroll *****');
+//   console.info('window.scrollX: ', window.scrollX);
+//   console.info('window.scrollY: ', window.scrollY);
+//   console.info('event - scroll: ', e);
+// });
 
-// Scroll Event (not working !!!)
+// Scroll Event (no funciona bien en Chrome, sí en Firefox !!!)
 window.onscroll= (e) => {
   console.info('scroll');
 }
+
+// Mousewheel Event (funciona en Chrome !!!)
+window.addEventListener('mousewheel', e => {
+  console.clear();
+  console.info('***** Evento Mousewheel *****');
+  console.info('window.scrollX: ', window.scrollX);
+  console.info('window.scrollY: ', window.scrollY);
+  console.info('event - mousewheel: ', e);
+});
 
 
 // Load Event  -> espera que carguen las imágenes, las hojas de estilo, las sub-ramas y las hojas de scripts, además de que el documento haya sido totalmente cargado y parseado. (tarda más que "DOMContentLoaded").
@@ -43,4 +51,46 @@ window.addEventListener('DOMContentLoaded', e => {
   console.info('window.screenY: ', window.screenY);
   console.info('event - DOMContentLoaded: ', e);
 });
+
+// Métodos de BOM
+
+const $btnAbrir = document.getElementById('abrir-ventana');
+const $btnCerrar = document.getElementById('cerrar-ventana');
+const $btnImprimir = document.getElementById('imprimir-ventana');
+
+const $btnAbrirMultiple = document.getElementById('abrir-multiple-ventana');
+const $btnCerrarMultiple = document.getElementById('cerrar-multiple-ventana');
+
+let ventana;
+
+$btnAbrir.addEventListener('click', e => {
+  ventana = open('https://jonmircha.com');
+  console.info('método - window.open(): ', e);
+});
+
+// Cierra solamente la última ventana abierta con el método open().
+$btnCerrar.addEventListener('click', e => {
+  ventana.close();
+  console.info('método - ventana.close(): ', e);
+});
+
+// Método que abre la ventana actual para mandar a la impresora (se puede generar una hoja de estilos CSS que se cargue cuando el usuario va a imprimir la página)
+$btnImprimir.addEventListener('click', e => {
+  print();
+});
+
+// Alternativa para cerrar todas las ventanas que se abrieron con el método open()
+
+let ventanas = [];
+
+$btnAbrirMultiple.addEventListener('click', e => {
+  ventanas.push(open('https://jonmircha.com'));
+  console.info('método - window.open(): ', e);
+});
+
+$btnCerrarMultiple.addEventListener('click', e => {
+  ventanas.forEach(ventana => ventana.close());
+  console.info('método - ventana.close(): ', e);
+});
+
 
